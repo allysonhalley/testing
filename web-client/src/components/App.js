c94f874163b1a3e47cfef148cdc9917e84d828f4
Allyson Halley <allysonhalley@gmail.com>
2021-10-16 16:34:45 -0300

    aula Controlando autenticação em rotas no ReactJS (Rocketseat)

---
+++ web-client/src/components/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Routes from '../routes';
+import Post from './Post';
+
+const App = () => <Routes />;
+
+// export default class App extends React.Component {
+//   render() {
+//     return (
+//       <div>
+//         <h1>Hello World with React!</h1>
+//         <Post title="Lutando com ReactJS" />
+//         <Post title="Quase perdendo" />
+//         <Post title="MISERICÓRDIA" />
+//       </div>
+      
+//     );
+//   }
+// }
+
+export default App;
